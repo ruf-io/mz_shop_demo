5502910ab02989119ead5c26235385eeb596930a
ruf-io <andy@materialize.com>
2021-03-15 12:15:00 -0400

    fix script

--- loadgen/main.go
+++ loadgen/main.go
@@ -10,6 +10,12 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+
+type item struct {
+    id    int
+    price float64
+}
+
 const (
 	userSeedCount      = 1000
 	itemSeedCount      = 200
@@ -18,11 +24,18 @@ const (
 	purchaseGenEveryMS = 100
 	item_inventory_min = 10
 	item_inventory_max = 1000
+	item_price_min     = 5.0
+	item_price_max     = 500.0
+)
+
+var (
+	items []item
 	first_names = [20]string{"Liam", "Olivia", "Noah", "Emma", "Oliver", "Ava", "William", "Sophia","Elijah", "Isabella", "James", "Charlotte", "Benjamin", "Amelia", "Lucas", "Mia", "Mason", "Harper", "Ethan", "Evelyn"}
 	last_names = [20]string{"Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez", "Hernandez", "Lopez", "Gonzalez", "Wilson", "Anderson", "Thomas", "Taylor", "Moore", "Jackson", "Martin"}
 	descriptors = [20]string{ "Adaptable", "Ambitious", "Brave", "Calm", "Cheerful", "Classic", "Cultured", "Delightful", "Delicate", "Familiar", "Fearless", "Gentle", "Harmonious", "Joyous", "Lovely", "Lucky", "Noble", "Original", "Timeless", "Wise" }
 	products = [15]string{"Fedora", "Boater", "Snapback", "Trilby", "Panama", "Bowler", "Dad", "Newsboy", "Flat Cap", "Beanie", "Bucket", "Baseball", "Trapper", "Pork Pie", "Top Hat"}
 )
+
 
 func doExec(db *sql.DB, str string) {
 	_, err := db.Exec(str)
@@ -61,25 +74,6 @@ func main() {
   			updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 		);`)
 
-	doExec(db, `CREATE TABLE shop.purchase
-		(
-			id SERIAL PRIMARY KEY,
-			user_id BIGINT UNSIGNED REFERENCES user(id),
-			status TINYINT UNSIGNED DEFAULT 1,
-			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  			updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-		);`)
-	
-	doExec(db, `CREATE TABLE shop.purchase_item
-		(
-			id SERIAL PRIMARY KEY,
-			purchase_id BIGINT UNSIGNED REFERENCES purchase(id),
-			item_id BIGINT UNSIGNED REFERENCES item(id),
-			quantity INT UNSIGNED DEFAULT 1,
-			purchase_price DECIMAL(12,2),
-			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-		);`)
-
 	doExec(db, `CREATE TABLE shop.item
 		(
 			id SERIAL PRIMARY KEY,
@@ -90,26 +84,36 @@ func main() {
   			updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 		);`)
 
+	doExec(db, `CREATE TABLE shop.purchase
+		(
+			id SERIAL PRIMARY KEY,
+			user_id BIGINT UNSIGNED REFERENCES user(id),
+			item_id BIGINT UNSIGNED REFERENCES item(id),
+			status TINYINT UNSIGNED DEFAULT 1,
+			quantity INT UNSIGNED DEFAULT 1,
+			purchase_price DECIMAL(12,2),
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  			updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+		);`)
+
 	insertItem := genPrepare(db,
 		"INSERT INTO shop.item (name, price, daily_inventory) VALUES ( ?, ?, ? );")
 	insertUser := genPrepare(db,
 		"INSERT INTO shop.user (name, email, is_vip) VALUES ( ?, ?, ? );")
 	insertPurchase := genPrepare(db,
-		"INSERT INTO shop.purchase (user_id) VALUES ( ? );")
-	insertPurchaseItem := genPrepare(db,
-		"INSERT INTO shop.purchase_item (purchase_id, item_id, quantity, purchase_price) VALUES ( ?, ?, ?, ? );")
-
+		"INSERT INTO shop.purchase (user_id, item_id, quantity, purchase_price) VALUES ( ?, ?, ?, ? );")
+	
 	rndSrc := rand.NewSource(time.Now().UnixNano())
 	rnd := rand.New(rndSrc)
 
 	
-	fmt.Println("Seeding %d shop items...", itemSeedCount)
+	fmt.Printf("Seeding %d shop items...", itemSeedCount)
 	for i := 0; i < itemSeedCount; i++ {
 		// Insert user with random region.
 		_, err = insertItem.Exec(
-			"The " + descriptors[rnd.Intn(len(descriptors))] + " " + products[rnd.Intn(len(products))],
+			fmt.Sprintf("The %s %s", descriptors[rnd.Intn(len(descriptors))], products[rnd.Intn(len(products))]),
 			rnd.Intn(item_price_max - item_price_min) + item_price_min,
-			rnd.Intn(item_inventory_max - item_inventory_min) + item_inventory_min
+			(rnd.Intn(item_inventory_max - item_inventory_min) + item_inventory_min),
 		)
 
 		if err != nil {
@@ -117,18 +121,19 @@ func main() {
 		}
 	}
 
-	fmt.Println("Seeding %d users...", userSeedCount)
+	fmt.Printf("Seeding %d users...", userSeedCount)
 	for i := 0; i < userSeedCount; i++ {
 		// Insert user with random name/email/is_vip status.
-		var user_name = first_names[rnd.Intn(len(first_names))] + " " + last_names[rnd.Intn(len(last_names))]
+		var user_name = fmt.Sprintf("%s %s", first_names[rnd.Intn(len(first_names))], last_names[rnd.Intn(len(last_names))])
+		var email = fmt.Sprintf("%s@%s", strings.Replace(user_name, " ", ".", -1), "gmail.com")
 		var is_vip = false
 		if rnd.Intn(10) == 9 {
 			is_vip = true
 		}
 		_, err = insertUser.Exec(
 			user_name,
-			strings.Replace(user_name, " ", ".") + "@gmail.com",
-			is_vip
+			email,
+			is_vip,
 		)
 
 		if err != nil {
@@ -137,7 +142,6 @@ func main() {
 	}
 
 	fmt.Println("Getting shop items...")
-	items = [][]
 	rows, err := db.Query("SELECT id, price FROM items;")
 	if err != nil {
 		// handle this error better than this
@@ -152,22 +156,15 @@ func main() {
 		// handle this error
 		panic(err)
 		}
-		items = append(items, [id, price])
+		items = append(items, item{id, price})
 	}
 	// get any error encountered during iteration
 	err = rows.Err()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("Loaded %d items", len(items))
 
-	fmt.Println("Creating purchases...")
-	for i := 0; i < purchaseSeedCount; i++ {
-		// Insert purchase with random amount and user.
-		_, err = insertPurchase.Exec(rnd.Float64()*10000, rnd.Intn(userIDCount)+userIDMin)
-		if err != nil {
-			panic(err.Error())
-		}
-	}
 
 	// Do some math to let users understand how fast data changes and how long
 	// the loadgen is set to run.
@@ -180,19 +177,19 @@ func main() {
 
 	// Continue generating purchases.
 	for i := 0; i < 10000; i++ {
-		_, err = insertPurchase.Exec(rnd.Float64()*10000, rnd.Intn(userIDCount)+userIDMin)
+		var purchase_item = items[rnd.Intn(len(items))]
+		var quantity = rnd.Intn(4) + 1
+		_, err = insertPurchase.Exec(
+			rnd.Intn(userSeedCount),
+			purchase_item.id,
+			quantity,
+			(purchase_item.price * float64(quantity)),
+		)
 
 		if err != nil {
 			panic(err.Error())
 		}
-
-		// Materialize requires some activity on row after source is created to
-		// ingest it. This frequency is overkill but was the simplest way to
-		// ensure some activity occurs at some indeterminate point after the
-		// loadgen starts.
-		timeNow := time.Now()
-		updateRegionTime.Exec(timeNow)
-		updateUserTime.Exec(timeNow)
+		
 		time.Sleep(sleepTime)
 	}
 
